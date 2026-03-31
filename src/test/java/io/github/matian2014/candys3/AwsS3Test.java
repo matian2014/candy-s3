772438c011784d3522f2b240924a07ebd47dcf71
connorma <matian1103@qq.com>
2026-03-31 15:30:04 +0800

    move CandyS3Test init method

--- src/test/java/io/github/matian2014/candys3/AwsS3Test.java
+++ src/test/java/io/github/matian2014/candys3/AwsS3Test.java
@@ -1,5 +1,8 @@
 package io.github.matian2014.candys3;
 
+import java.io.IOException;
+import java.util.Map;
+
 /**
  * AWS provider test entry.
  *
@@ -10,6 +13,17 @@ public class AwsS3Test extends CandyS3Test {
     @Override
     protected S3Provider provider() {
         return S3Provider.AWS;
+    }
+
+    @Override
+    protected CandyS3 init(S3Provider provider) throws IOException {
+        CandyS3 candyS3 = new CandyS3(provider);
+        Map<String, String> properties = readIni("aws.ini");
+        candyS3.setAccessKey(properties.get("access-key"));
+        candyS3.setSecretKey(properties.get("secret-key"));
+        candyS3.setRegion(properties.get("default-region"));
+        candyS3.setUseSSL(true);
+        return candyS3;
     }
 
     @Override
--- src/test/java/io/github/matian2014/candys3/TencentCloudCosS3Test.java
+++ src/test/java/io/github/matian2014/candys3/TencentCloudCosS3Test.java
@@ -1,5 +1,8 @@
 package io.github.matian2014.candys3;
 
+import java.io.IOException;
+import java.util.Map;
+
 /**
  * Tencent Cloud COS provider test entry.
  *
@@ -10,6 +13,18 @@ public class TencentCloudCosS3Test extends CandyS3Test {
     @Override
     protected S3Provider provider() {
         return S3Provider.TENCENTCLOUD_COS;
+    }
+
+    @Override
+    protected CandyS3 init(S3Provider provider) throws IOException {
+        CandyS3 candyS3 = new CandyS3(provider);
+        Map<String, String> properties = readIni("tencentcloud_cos.ini");
+        candyS3.setAccessKey(properties.get("access-key"));
+        candyS3.setSecretKey(properties.get("secret-key"));
+        candyS3.setRegion(properties.get("default-region"));
+        super.TENCENTCLOUD_COS_APPID = properties.get("cos-app-id");
+        candyS3.setUseSSL(true);
+        return candyS3;
     }
 
     @Override
